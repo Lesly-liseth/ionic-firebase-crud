b238c9b407417f5b1c033df9a94c51ad1cfdf425
Lesly <leslyherrera851@gmail.com>
2023-06-07 16:04:25 -0500

    Creacion de crud
    se empezo a crear la operación CRUD con AngularFire

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDV1PVriKPgTpXF90bo0EtcEmQFcnBHgrs",
+    authDomain: "geolocation-app-2d78c.firebaseapp.com",
+    databaseURL: "https://geolocation-app-2d78c-default-rtdb.firebaseio.com",
+    projectId: "geolocation-app-2d78c",
+    storageBucket: "geolocation-app-2d78c.appspot.com",
+    messagingSenderId: "574384683860",
+    appId: "1:574384683860:web:323ffd7edfbfc32064e6c1",
+    measurementId: "G-BCBPQ8EXYS"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
